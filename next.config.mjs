0fe36d476e51ea4e979ec938c4ba9a8c70563df9
Muhammet Eren <115183696+Merenylmz@users.noreply.github.com>
2024-07-25 14:58:42 +0300

    DockerConfiguration

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // output: "standalone",
     env: {
         apiLink: "http://localhost:8181/api",
         imageLink: "http://localhost:8181/storage"
